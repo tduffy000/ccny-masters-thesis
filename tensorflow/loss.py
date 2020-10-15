3de0171f24bdcea76a09674ad3205367e7ab11a2
Thomas Duffy <tduffy000@citymail.cuny.edu>
2020-10-15 09:50:22 -0400

    a simpler E2E loss for testing

---
+++ tensorflow/loss.py
@@ -0,0 +1,57 @@
+# Wan, L., Wang, Q., Papir, A., Lopez Moreno, I., __Generalized End-to-End Loss for Speaker Verification__
+# @see https://arxiv.org/abs/1710.10467
+import tensorflow as tf
+import numpy as np
+
+def get_embedding_loss(N, M):
+    """
+    Args:
+        N: The number of unique speakers in a batch.
+        M: The number of utterances from each speaker in a batch.
+    """
+    # cosine similarity is 1.0 if from same speaker, otherwise 0
+    S_true = np.zeros((N*M, N))
+    for i in range(N):
+        S_true[i*M:i*M+M,i] = 1.0
+
+    def loss(_, S):
+        # Eq (6) & Eq (10)
+        # print(f'S: {S}')
+        # l = tf.math.reduce_sum(
+        #     -S_true + tf.math.log(tf.math.reduce_sum(tf.exp(S), axis=1, keepdims=True) + 1e-6)
+        # )
+        # print(f'loss score: {l}')
+        l = tf.math.reduce_sum(
+            tf.abs(S - S_true)
+        )
+        return l
+    return loss
+
+def false_acceptance_ratio(S_thres, N, M):
+    """
+    S_thres [NM x N]
+    The ratio of falsely accepted impostor speakers over all scored impostors (type II errors).
+    """
+    total_fp = sum([ np.sum(S_thres[i,:]) - np.sum(S_thres[i,i:i+M]) for i in range(N)])
+    return total_fp/(N-1*M)
+
+def false_rejection_ratio(S_thres, N, M):
+    """
+    The ratio of falsely rejected geniune speakers over all genuine speakers (type I errors).
+    """
+    total_fn = sum([M - np.sum(S_thres[i,i:i+M]) for i in range(N)])
+    return total_fn/(N*M)
+
+def equal_error_ratio(S, N, M, threshold_start=0.5, threshold_step=0.01, iters=50):
+    """
+    The ratio at which FAR and FRR (defined above) are equivalent.
+    """
+    epsilon = 1.0
+    for threshold in [threshold_step*i + threshold_start for i in range(iters)]:
+        S_thres = S > threshold
+        far = false_acceptance_ratio(S_thres, N, M)
+        frr = false_rejection_ratio(S_thres, N, M)
+
+        if abs(far-frr) < epsilon:
+            return threshold, (far+frr)/2
+    return None
---
+++ tensorflow/loss_test.py
@@ -0,0 +1,32 @@
+import numpy as np
+from loss import get_embedding_loss
+
+N, M = 3, 2
+# [6,3]
+# where the correct scores are:
+# [[1, 0, 0],
+#  [1, 0, 0],
+#  [0, 1, 0],
+#  [0, 1, 0],
+#  [0, 0, 1],
+#  [0, 0, 1]]
+
+S_all_right = np.zeros((N*M, N))
+for i in range(N):
+    S_all_right[i*M:i*M+M,i] = 1.0
+
+S_all_wrong = np.ones((N*M, N))
+for i in range(N):
+    S_all_wrong[i*M:i*M+M,i] = 0.0
+
+### TESTS ###
+def test_all_right_score(caplog):
+    caplog.set_level(logging.INFO)
+    expected_score = 0.0
+    l = get_embedding_loss(N, M)(None, S_all_right)
+    assert l.numpy() == expected_score
+
+def test_all_wrong_score():
+    expected_score = N*N*M
+    l = get_embedding_loss(N, M)(None, S_all_wrong)
+    assert l.numpy() == expected_score
