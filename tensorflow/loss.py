fix the loss calculation

tensorflow/loss.py
@@ -9,14 +9,12 @@ def get_embedding_loss(N, M):
         N: The number of unique speakers in a batch.
         M: The number of utterances from each speaker in a batch.
     """
-    S_true = -np.ones((N*M, N))
-    for i in range(N):
-        S_true[i*M:i*M+M,i] = 1.0
-
     def loss(_, S):
         # Eq (6) & Eq (10)
+        S_correct = tf.concat([S[i*M:(i+1)*M, i:(i+1)] for i in range(N)], axis=0)
+
         l = tf.math.reduce_sum(
-            -S_true + tf.math.log(tf.math.reduce_sum(tf.exp(S), axis=1, keepdims=True) + 1e-6)
+            -S_correct + tf.math.log(tf.math.reduce_sum(tf.exp(S), axis=1, keepdims=True) + 1e-6)
         )
         return l
     return loss