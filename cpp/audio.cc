71391a7b9b40d661c9c23011dba00302e63bbe2d
Thomas Duffy <tduffy000@citymail.cuny.edu>
2020-11-02 19:30:16 -0500

    got there with FFT

--- cpp/audio.cc
+++ cpp/audio.cc
@@ -38,14 +38,13 @@ class IOHandler {
             return raw;
         };
 
-        // DRY this up, us the iterator template
-        static void write(std::valarray<std::valarray<float>> mat, std::string path) {
+        static void write(std::valarray<std::valarray<float>> frames, std::string path) {
             std::ofstream out_file;
             std::ostream_iterator<float> out_it (out_file, ",");
 
             out_file.open (path);
-            for (const auto& row : mat) {
-                std::copy(std::begin(row), std::end(row), out_it);
+            for (const auto& f : frames) {
+                std::copy(std::begin(f), std::end(f), out_it);
                 out_file << std::endl;
             }
             out_file.close();
@@ -65,34 +64,15 @@ class IOHandler {
 
 };
 
-class AudioPreparer {
+class AudioFeatures {
 
     public:
         typedef std::complex<float> Complex;
         typedef std::valarray<float> Waveform;
-        typedef std::valarray<std::valarray<float>> Matrix;
+        typedef std::valarray<std::valarray<Complex>> ComplexMatrix;
+        typedef std::valarray<std::valarray<float>> RealMatrix;
 
-        // https://www.oreilly.com/library/view/c-cookbook/0596007612/ch11s18.html        
-        // https://rosettacode.org/wiki/Fast_Fourier_transform#C.2B.2B
-        // https://cp-algorithms.com/algebra/fft.html
-        // https://github.com/numpy/numpy/blob/92ebe1e9a6aeb47a881a1226b08218175776f9ea/numpy/fft/_pocketfft.py#L287
-        static void fft(std::valarray<Complex>& x) {
-            const size_t N = x.size();
-            if (N <= 1) return;
-
-            std::valarray<Complex> even = x[std::slice(0, N/2, 2)];
-            std::valarray<Complex> odd = x[std::slice(1, N/2, 2)];
-        
-            fft(even);
-            fft(odd);
-
-            for(size_t k = 0; k < N/2; ++k) {
-                Complex t = std::polar(1.0f, -2 * PI * k / N) * odd[k];
-                x[k] = even[k] + t;
-                x[k+N/2] = even[k] - t;
-            };
-        };
-
+        /** WINDOWING */
         static std::vector<Waveform> frame(Waveform wv, int win_length, int hop_length) {
             int offset = 0;
             std::vector<Waveform> frames;
@@ -119,66 +99,140 @@ class AudioPreparer {
             return padded;
         }
 
+        /** Fourier transform */
+        static RealMatrix raise(ComplexMatrix& mat, float power) {
+            RealMatrix m (mat.size());
+            for (int i = 0; i < mat.size(); i++) {
+                std::valarray<float> x (mat[i].size());
+                for (int j = 0; j < mat[i].size(); j++) {
+                    x[j] = std::pow(std::abs(mat[i][j]), power);
+                }
+                m[i] = x;
+            }
+            return m;       
+        }
+
+        static RealMatrix magnitude(ComplexMatrix mat) {
+            return raise(mat, 1.0f);
+        };
+
+        static RealMatrix power(ComplexMatrix mat) {
+            return raise(mat, 2.0f);
+        };
+
+        // http://www.robots.ox.ac.uk/~sjrob/Teaching/SP/l7.pdf
+        // https://www.princeton.edu/~cuff/ele201/files/spectrogram.pdf
+        static std::valarray<Complex> dft(std::valarray<Complex> &in) {
+            Complex J(0, 1);
+            const size_t N = in.size(); 
+            std::valarray<Complex> out (N);
+
+            for (size_t k = 0; k < N; k++) {
+                Complex s(0, 0);
+                for (size_t t = 0; t < N; t++) {
+                    float angle = 2 * PI * t * k / N;
+                    s += in[t] * std::exp(Complex(0, -angle));
+                }
+                out[k] = s;
+            }
+            return out;
+        };
+
+        // https://rosettacode.org/wiki/Fast_Fourier_transform#C.2B.2B
+        // https://cp-algorithms.com/algebra/fft.html
+        // https://github.com/numpy/numpy/blob/92ebe1e9a6aeb47a881a1226b08218175776f9ea/numpy/fft/_pocketfft.py#L287
+        static void fft(std::valarray<Complex>& x) {
+            const size_t N = x.size();
+            if (N <= 1) return;
+
+            std::valarray<Complex> even = x[std::slice(0, N/2, 2)];
+            std::valarray<Complex> odd = x[std::slice(1, N/2, 2)];
+        
+            fft(even);
+            fft(odd);
+
+            for(size_t k = 0; k < N/2; ++k) {
+                Complex t = std::polar(1.0f, -2 * PI * k / N) * odd[k];
+                x[k] = even[k] + t;
+                x[k+N/2] = even[k] - t;
+            };
+        };
+
         // https://github.com/librosa/librosa/blob/a53fa56bdb6695a994008d5b6ccd0100870a6036/librosa/core/spectrum.py#L42
-        static Matrix stft(std::vector<Waveform>& windows, int nfft = 512, float power = 1.0f) {
+        static ComplexMatrix stft(std::vector<Waveform>& windows, int nfft = 512) {
             
             bool to_pad = windows[0].size() < nfft;
             int length = to_pad ? nfft : windows[0].size();
 
-            Matrix m (windows.size());
+            ComplexMatrix m (windows.size());
 
             int offset = to_pad ? (nfft - windows[0].size())/2 : 0;
             for (int i = 0; i < windows.size(); i++) {
 
                 Waveform padded = pad(windows[i], offset);
                 hamming(padded, nfft);
+
                 std::valarray<Complex> complex_window (padded.size());
-                for(int i = 0; i < padded.size(); i++) {
-                    complex_window[i] = (Complex) padded[i];
+                for(int j = 0; j < padded.size(); j++) {
+                    complex_window[j] = (Complex) padded[j];
                 }
                 fft(complex_window);
-
-                std::valarray<float> real_part (complex_window.size());
-                for (int i = 0; i < complex_window.size(); i++) {
-                    real_part[i] = std::abs(complex_window[i]);
-                }
-                m[i] = real_part;
+                // only need the left side because for real-valued signal, transform is reflection-symmetric
+                std::valarray<Complex> real_side = complex_window[std::slice(0, 1 + (nfft/2), 1)];
+                m[i] = real_side;
             }
-
             return m;
         };
 
-        // https://github.com/librosa/librosa/blob/master/librosa/feature/spectral.py#L1873
-        static void filter_bank() {};
+        /** Filter banks */
+        static std::valarray<float> mel_filters(int nfilter, int sr) {
+            float low_freq = 0.0f;
+            float high_freq = (2595 * std::log10(1 + (sr / 2)/ 700));
+            float step = (high_freq - low_freq) / nfilter;
 
-        static void mfcc() {};
+            std::valarray<float> filters (0.0f, nfilter+2);
+            for (int i = 1; i < filters.size(); i++) {
+                filters[i] = filters[i-1] + step;
+            }
+            return filters;
+        };
+
+        static std::valarray<float> mel_to_hz(std::valarray<float> filts) {
+            return (700 * (std::pow(filts / 2595.0f, 10) - 1));
+        }
+
+        // https://github.com/librosa/librosa/blob/master/librosa/feature/spectral.py#L1873
+        // static RealMatrix filter_bank(RealMatrix m) {
+
+        // };
+
+        /** MFCCs */
+        // static RealMatrix mfcc(RealMatrix m) {
+
+        // };
 
 };
 
 int main() {
 
     std::string wave_path = "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/sample_wave.out";
-    AudioPreparer::Waveform raw_wave = IOHandler::load(wave_path);
+    AudioFeatures::Waveform raw_wave = IOHandler::load(wave_path);
 
     // test framing
-    std::vector<AudioPreparer::Waveform> frames = AudioPreparer::frame(raw_wave, WIN_LENGTH, HOP_LENGTH);
+    std::vector<AudioFeatures::Waveform> frames = AudioFeatures::frame(raw_wave, WIN_LENGTH, HOP_LENGTH);
     IOHandler::write(frames, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/orig_frames.txt");
 
-    // test Hamming window
-    // for (auto& frame : frames) {
-    //     AudioPreparer::hamming(frame, WIN_LENGTH);
-    // }
-    // IOHandler::write(frames, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/hamming_frames.txt");
-
     // test stft
-    AudioPreparer::Matrix m = AudioPreparer::stft(frames, N_FFT);
-    IOHandler::write(m, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/stft_frames.txt");
+    AudioFeatures::ComplexMatrix m = AudioFeatures::stft(frames, N_FFT);
+    AudioFeatures::RealMatrix magnitude_mat = AudioFeatures::magnitude(m);
+    IOHandler::write(magnitude_mat, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/stft_magnitude_frames.txt");
+
+    AudioFeatures::RealMatrix power_mat = AudioFeatures::power(m);
+    IOHandler::write(power_mat, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/stft_power_frames.txt");
 
     // test filter_bank
-    // for (auto& frame : frames) {
-    //     AudioPreparer::filter_bank();
-    // }
-    // IOHandler::write(frames, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/filter_bank.txt");
+    // AudioPreparer::RealMatrix fb = AudioPreparer::filter_bank(mat);
+    // IOHandler::write(fb, "/home/thomas/Dir/ccny/ccny-masters-thesis/cpp/filter_bank.txt");
 
     // // test mfcc
     // for (auto& frame : frames) {
