f1b85c95184c9542d9a5f74f208c109a5782ce7d
Thomas Duffy <tduffy000@citymail.cuny.edu>
2020-11-08 10:33:00 -0500

    extractor for MFCC

--- tensorflow/features.py
+++ tensorflow/features.py
@@ -1,7 +1,6 @@
 import librosa
 import numpy as np
 
-# TODO: some add the 1st and 2nd derivatives by way of librosa.feature.delta
 class FeatureExtractor:
 
     def __init__(
@@ -17,6 +16,20 @@ class FeatureExtractor:
         self.window_length = int(window_length * sr)
         self.overlap = int(self.window_length * overlap_percent)
         self.trim_top_db = trim_top_db
+
+    @staticmethod
+    def validate_numeric(feature):
+        nans_found = np.sum(np.isnan(feature))
+        infs_found = np.sum(np.isinf(feature))
+        if nans_found > 0 or infs_found > 0:
+            logger.fatal('Found a NaN or Infinity!')
+            raise ValueError('Features must have real-valued elements.')
+
+    def get_feature_shape(self, path):
+        try:
+            return next(self.as_features(path)).shape
+        except StopIteration:
+            return None
 
     def get_intervals(self, path):
         """
@@ -67,20 +80,6 @@ class SpectrogramExtractor(FeatureExtractor):
         self.n_mels=n_mels
         self.log_lift = 1e-6
 
-    def get_feature_shape(self, path):
-        try:
-            return next(self.as_features(path)).shape
-        except StopIteration:
-            return None
-
-    @staticmethod
-    def validate_numeric(feature):
-        nans_found = np.sum(np.isnan(feature))
-        infs_found = np.sum(np.isinf(feature))
-        if nans_found > 0 or infs_found > 0:
-            logger.fatal('Found a NaN or Infinity!')
-            raise ValueError('Features must have real-valued elements.')
-
     def as_features(self, path):
         _, windows = self.get_intervals(path)
         for w in windows:
@@ -94,3 +93,42 @@ class SpectrogramExtractor(FeatureExtractor):
             )
             self.validate_numeric(feature)
             yield np.log10(feature + self.log_lift)
+
+class MFCCExtractor(FeatureExtractor):
+
+    def __init__(
+            self,
+            window_length,   # in seconds
+            overlap_percent, # in percent
+            frame_length,    # in seconds
+            hop_length,      # in seconds
+            sr=16000,
+            n_fft=512,
+            n_mfcc=20,
+            trim_top_db=30
+        ):
+            super().__init__(
+                window_length=window_length,
+                overlap_percent=overlap_percent,
+                sr=sr,
+                trim_top_db=trim_top_db
+            )
+            self.hop_length = int(hop_length * sr)
+            self.frame_length = int(frame_length * sr)
+            self.n_fft=n_fft
+            self.n_mfcc=n_mfcc
+            self.log_lift = 1e-6
+
+    def as_features(self, path):
+    _, windows = self.get_intervals(path)
+    for w in windows:
+        feature = librosa.feature.mfcc(
+            w,
+            sr=self.sr,
+            n_fft=self.n_fft,
+            n_mfcc=self.n_mfcc,
+            win_length=self.frame_length,
+            hop_length=self.hop_length
+        )
+        self.validate_numeric(feature)
+        yield np.log10(feature + self.log_lift)
